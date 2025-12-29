b97a7571e6a03f202050f63286b492621e744bde
Julien Virlogeux <julien.virlogeux@protonmail.com>
2025-12-29 16:09:33 +0100

    Add CI/CD workflow

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
   // If your repo is "my-website", this must be '/my-website/'
   base: '/', 
 
-  // 2. Server Settings (Optional)
-  server: {
-    port: 3000,       // You can force it to use port 3000
-    open: true,       // Open browser automatically when you start
-  },
-
   // 3. Build Settings (Optional)
   build: {
     outDir: 'dist',   // Where the files go (default is 'dist')
